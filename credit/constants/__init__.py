fc990368604f2f29e8ec6eebb6b61f838c07b26a
Somesh140 <somesh140@gmail.com>
2022-10-15 17:30:14 +0530

    recall key added

--- credit/constants/__init__.py
+++ credit/constants/__init__.py
@@ -60,7 +60,7 @@ MODEL_TRAINER_ARTIFACT_DIR = "model_trainer"
 MODEL_TRAINER_CONFIG_KEY = "model_trainer_config"
 MODEL_TRAINER_TRAINED_MODEL_DIR_KEY = "trained_model_dir"
 MODEL_TRAINER_TRAINED_MODEL_FILE_NAME_KEY = "model_file_name"
-MODEL_TRAINER_BASE_ACCURACY_KEY = "base_accuracy"
+MODEL_TRAINER_BASE_RECALL_KEY = "base_recall"
 MODEL_TRAINER_MODEL_CONFIG_DIR_KEY = "model_config_dir"
 MODEL_TRAINER_MODEL_CONFIG_FILE_NAME_KEY = "model_config_file_name"
 
