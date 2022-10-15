102c5a4bab327472e24944be75903582b7346c83
Somesh140 <somesh140@gmail.com>
2022-10-15 16:43:17 +0530

    Modeltrainierartifactclassifier updated

--- credit/entity/artifact_entity.py
+++ credit/entity/artifact_entity.py
@@ -11,17 +11,19 @@ DataTransformationArtifact = namedtuple("DataTransformationArtifact",
  ["is_transformed", "message", "transformed_train_file_path","transformed_test_file_path",
      "preprocessed_object_file_path"])
 
-ModelTrainerArtifact = namedtuple("ModelTrainerArtifact", 
+"""ModelTrainerArtifact = namedtuple("ModelTrainerArtifact", 
                             ["is_trained", "message", "trained_model_file_path",
                             "train_rmse", "test_rmse", "train_accuracy", "test_accuracy",
-                              "model_accuracy"])
+                              "model_accuracy"])"""#used in regressionproject
+
+
+ModelTrainerArtifactClassifier = namedtuple("ModelTrainerArtifactClassifier", 
+                            ["is_trained", "message", "trained_model_file_path",
+                            "train_roc_auc", "test_roc_auc", "train_accuracy", "test_accuracy",
+                              "model_accuracy","train_recall","test_recall","model_recall"])
 
 ModelEvaluationArtifact = namedtuple("ModelEvaluationArtifact",
                          ["is_model_accepted", "evaluated_model_path"])
 
 ModelPusherArtifact = namedtuple("ModelPusherArtifact", ["is_model_pushed", "export_model_file_path"])
 
-ModelTrainerArtifactClassifier = namedtuple("ModelTrainerArtifactClassifier", 
-                            ["is_trained", "message", "trained_model_file_path",
-                            "train_roc_auc", "test_roc_auc", "train_accuracy", "test_accuracy",
-                              "model_accuracy"])
